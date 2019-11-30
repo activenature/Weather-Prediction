d62d8ca7afdb7b81edbf412e6a9c84d34a5b213c
bigtree <bigtree@163.com>
2019-11-29 23:47:14 -0500

    new commit in midnight

---
+++ src/cn/com/webxml/GoodDay.java
@@ -0,0 +1,11 @@
+package cn.com.webxml;
+
+public class GoodDay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Today is a good day.");
+
+	}
+
+}
